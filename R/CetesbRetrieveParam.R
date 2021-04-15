8057e52e17a6c1a74d451a52f9a75571cc982c26
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2021-04-15 18:42:08 -0300

    Add aqs_code check and query summary to CetesbRetrieveParam

--- R/CetesbRetrieveParam.R
+++ R/CetesbRetrieveParam.R
@@ -10,6 +10,7 @@
 #' @param aqs_code Code of AQS
 #' @param start_date Date to start downloading in dd/mm/yyyy
 #' @param end_date Date to end downloading in dd/mm/yyyy
+#' @param verbose Print query summary
 #'
 #' @return data.frame with parameters described in `params` vector
 #' @export
@@ -31,15 +32,41 @@
 #'                                  start_date, end_date)
 #' }
 CetesbRetrieveParam <- function(username, password, params,
-                                aqs_code, start_date, end_date){
+                                aqs_code, start_date, end_date,
+                                verbose = TRUE){
+  aqs <- cetesb
+
+  # Check if aqs_code is valid
+  if (is.numeric(aqs_code) & aqs_code %in% aqs$code){
+    aqs_code <- aqs_code
+    aqs_name <- aqs$name[aqs$code == aqs_code]
+  } else if (is.character(aqs_code) & aqs_code %in% (aqs$name)){
+    aqs_name <- aqs_code
+    aqs_code <- aqs$code[aqs$name == aqs_code]
+  } else {
+    stop("Wrong aqs_code value, please check cetesb_latlon or cetesb_aqs",
+         call. = FALSE)
+  }
+
   param <- toupper(params)
   codes_df <- params_code[params_code$name %in% param, ]
+
+  # Adding query summary
+  if (verbose){
+    cat("Your query is:\n")
+    cat("Parameter:", paste(param, collapse = ", "), "\n")
+    cat("Air quality staion:", aqs_name, "\n")
+    cat("Period: From", start_date, "to", end_date, "\n")
+  }
+
+  # Downloading data
   aqs_data <-  lapply(codes_df$code, CetesbRetrieve,
                       username = username,
                       password = password,
                       aqs_code = aqs_code,
                       start_date = start_date,
-                      end_date = end_date)
+                      end_date = end_date,
+                      verbose = FALSE)
 
   for (i in seq(length(aqs_data))){
     names(aqs_data[[i]]) <- c("date", tolower(codes_df$name[i]), "aqs")
