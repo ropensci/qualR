f7f80553c0fdc063d65cee7dece6fec68dd35c07
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-01-26 21:40:25 -0300

    Add csv_path in test where to_csv = T

--- tests/testthat/test-CetesbRetrieveParam.R
+++ tests/testthat/test-CetesbRetrieveParam.R
@@ -1,16 +1,16 @@
 test_that("CetesbRetrieveParam works!", {
 
-  current_folder <- getwd()
-  on.exit(setwd(current_folder), add = T)
+  # current_folder <- getwd()
+  # on.exit(setwd(current_folder), add = T)
 
   load("sysdata.rda")
 
-  dir.create(file.path(tempdir(), "cetesb-data2"))
-  setwd(file.path(tempdir(), "cetesb-data2"))
+  csv_path <- dir.create(file.path(tempdir(), "cetesb-data2"))
+  # setwd(file.path(tempdir(), "cetesb-data2"))
 
   pin_param <- CetesbRetrieveParam(u,p,c("o3","NOX", "VV"), 99,
                                    "01/01/2020", "07/01/2020",
-                                   to_csv = TRUE)
+                                   to_csv = TRUE, csv_path = csv_path)
 
   expect_equal(2 * 2, 4)
 })
--- tests/testthat/test-MonitorArRetrieve.R
+++ tests/testthat/test-MonitorArRetrieve.R
@@ -1,16 +1,16 @@
 test_that("MonitorArRetrieve works", {
 
-  current_folder <- getwd()
-  on.exit(setwd(current_folder), add = T)
+  # current_folder <- getwd()
+  # on.exit(setwd(current_folder), add = T)
 
-  dir.create(file.path(tempdir(), "cetesb-data"))
-  setwd(file.path(tempdir(), "cetesb-data"))
+  csv_path <- dir.create(file.path(tempdir(), "cetesb-data"))
+  # setwd(file.path(tempdir(), "cetesb-data"))
 
   ca_o3 <- MonitorArRetrieveParam(start_date = "01/02/2015",
                                   end_date   = "01/03/2015",
                                   aqs_code   = "CA",
                                   parameters = "O3",
-                                  to_csv     = TRUE)
+                                  to_csv     = TRUE, csv_path = csv_path)
 
   expect_equal(ncol(ca_o3),3)
 })
