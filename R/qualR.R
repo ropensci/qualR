30b61db4b926c5b0d86c2b249ce758bbfc43c8a2
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-01-30 22:39:59 -0300

    Add qualR.R

---
+++ R/qualR.R
@@ -0,0 +1,18 @@
+#' qualR: An R package to download Sao Paulo and Rio de Janeiro
+#' air pollution data
+#'
+#' @name qualR
+#' @author Mario Gavidia-Calderon and Daniel Schuch and Maria de Fatima Andrade
+#' @docType package
+#'
+#' @seealso
+#'\strong{Useful links:}
+#' \itemize{
+#' \item{Development repository:
+#'  \url{https://github.com/quishqa/qualR}}
+#' \item{Report bugs:
+#'  \url{https://github.com/quishqa/qualR/issues}}
+#' \item{qualR site:
+#'  \url{https://quishqa.github.io/qualR/}}
+#' }
+"_PACKAGE"
