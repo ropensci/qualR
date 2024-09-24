366d048dd702a4b42eb658da84a01c472933a1bd
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2024-09-23 22:13:38 -0300

    fix: update cetesb aqs internal

--- data-raw/cetesb.R
+++ data-raw/cetesb.R
@@ -1,10 +1,10 @@
 ## code to prepare internal datasets goes here
 
-cetesb <- read.table("~/R_tests/QualR/qualR/cetesb_qualR.dat", sep = ",",
-                     header = FALSE, col.names = c("code", "name"),
+cetesb <- read.table("cetesb_qualR.dat", sep = ",",
+                     header = TRUE, col.names = c("code", "name"),
                      stringsAsFactors = FALSE)
 cetesb$ascii <- iconv(cetesb$name, from = "UTF-8", to = "ASCII//TRANSLIT")
-params <- read.table("~/R_tests/cetesb_variables.dat", header = FALSE,
+params <- read.table("cetesb_variables.dat", header = TRUE,
                      sep = ",", col.names = c("code", "name"),
                      stringsAsFactors = FALSE)
 
@@ -18,28 +18,12 @@ params_code <- data.frame(
 )
 
 # Monitor Ar data
-param_code <- c("SO2", "NO2", "NO", "NOx", "HCNM", "HCT",
-                "CH4", "CO", "O3", "PM10", "PM2_5",
-                "Chuva", "Pres", "RS", "Temp", "UR",
-                "Dir_Vento", "Vel_Vento")
-param_name <- c("Dioxido de enxofre", "Dioxido de nitrogenio", "Monoxido de Nitrogenio", "Oxidos de nitrogenio",
-                "Hidrocarbonetos Totais menos Metano", "Hidrocarbonetos Totais", "Metano", "Monoxido de Carbono",
-                "Ozonio", "Particulas Inalaveis", "Particulas Inalaveis Finas", "Precipitacao Pluviometrica",
-                "Pressao Atmosferica", "Radiacao Solar", "Temperatura", "Umidade Relativa do Ar",
-                "Direcao do Vento", "Velocidade do Vento")
-param_units <- c("ug/m3", "ug/m3", "ug/m3", "ug/m3", "ppm", "ppm",
-                 "ug/m3", "ppm", "ug/m3", "ug/m3", "ug/m3",
-                 "mm", "mbar", "W/m2", "ºC", "%",
-                 "º", "m/s")
+param_monitor_ar <- read.table("monitor_ar_variables.dat",
+                               header = TRUE, sep = ",",
+                               stringsAsFactors = FALSE)
 
-param_monitor_ar <- data.frame(code = param_code,
-                               name = param_name,
-                               units = param_units)
-
-aqs_monitor_ar <- read.table("~/Downloads/aqs_rio.csv", header = TRUE, stringsAsFactors = FALSE,
+aqs_monitor_ar <- read.table("monitor_ar_qualR.dat", header = TRUE, stringsAsFactors = FALSE,
                              sep = ",", dec=".")
-aqs_monitor_ar <- aqs_monitor_ar[, c(4, 8, 1, 2, 12, 13)]
-names(aqs_monitor_ar) <- c("name", "code", "lon", "lat", "X_UTM_Sirgas2000", "Y_UTM_Sirgas2000")
 aqs_monitor_ar$name <- iconv(aqs_monitor_ar$name, from = "UTF-8", to = "ASCII//TRANSLIT")
 
 
